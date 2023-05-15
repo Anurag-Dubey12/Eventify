361a3d2838a18d36fab33525fbc6dfed68cc0a83
Anurag Dubey <126979723+BackSlash-Anurag@users.noreply.github.com>
2023-05-15 18:21:34 +0530

    Navigation ui done

--- app/src/main/java/com/example/eventmatics/Navigation/Navigation.kt
+++ app/src/main/java/com/example/eventmatics/Navigation/Navigation.kt
@@ -1,17 +1,17 @@
-package com.example.eventmatics
+package com.example.eventmatics.Navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.eventmatics.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class Navigation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val bottomnav: BottomNavigationView =findViewById(R.id.bottomnav)
+        setContentView(R.layout.activity_navigation)
+        val bottomnav: BottomNavigationView=findViewById(R.id.bottomnav)
         bottomnav.background=null
         bottomnav.menu.getItem(2).isEnabled=false
-
 
     }
 }
